c7f1adc43e9bb6e154c40dc42ad8afb96c4709fc
Soufiene AIT ALI <fatfataassr@gmail.com>
2023-05-06 11:01:45 +0100

    correction

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,4 +1,3 @@
-import HomePageGraphic from "../../assets/HomePageGraphic.png";
 import SponsorRedBull from "../../assets/SponsorRedBull.png";
 import SponsorForbes from "../../assets/SponsorForbes.png";
 import SponsorFortune from "../../assets/SponsorFortune.png";
